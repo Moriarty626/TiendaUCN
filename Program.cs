00349714c4183bdd31757e7e2f51249c6d990a54
Gladys-A <gladys.araya@alumnos.ucn.cl>
2026-04-04 11:41:31 -0300

    feat(serilog): add and configure serilog

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.AspNetCore.Identity.Data;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+
+#region Logging Configuration
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services));
+#endregion
+
 var app = builder.Build();
 
-app.MapOpenApi();
 
+
+app.MapOpenApi();
 
 app.Run();
